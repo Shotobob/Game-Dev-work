e24a8ae8d51b7ca1334401c59fb1410c519498b6
Shotobob <109097353+Shotobob@users.noreply.github.com>
2025-09-26 11:50:39 -0500

    ww

--- Tower of Hanoi/Assets/Script/TullyMonster67.cs
+++ Tower of Hanoi/Assets/Script/TullyMonster67.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class TullyMonster67 : MonoBehaviour
 {
+    [SerializeField] List<Pole> poles;
+    public int selectedPole = -1;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,5 +15,27 @@ public class TullyMonster67 : MonoBehaviour
     void Update()
     {
         
+    }
+    public void poleClicked(int pole)
+    {
+        if(selectedPole == -1)
+        {
+            selectedPole = pole;
+            poles[pole].GetComponent<Pole>().setSelected(true);
+            poles[pole].GetComponent<Pole>().setColor();
+        }
+        else if(selectedPole == pole)
+        {
+            selectedPole = -1;
+            poles[pole].GetComponent<Pole>().setSelected(false);
+            poles[pole].GetComponent<Pole>().setColor();
+        }
+        else{
+            poles[selectedPole].GetComponent<Pole>().setSelected(false);
+            poles[selectedPole].GetComponent<Pole>().setColor();
+            selectedPole = pole;
+            poles[pole].GetComponent<Pole>().setSelected(true);
+            poles[pole].GetComponent<Pole>().setColor();
+        }
     }
 }
