d7fd054fa250352e107c1586eef0917904ac851e
Shotobob <109097353+Shotobob@users.noreply.github.com>
2025-09-26 11:50:35 -0500

    wrg

--- Tower of Hanoi/Assets/Script/Pole.cs
+++ Tower of Hanoi/Assets/Script/Pole.cs
@@ -1,9 +1,13 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class Pole : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] GameObject manager;
+    [SerializeField] List<Rings> rings;
+    [SerializeField] int poleNum = 0;
+    public bool selected = false;
 
 
     void Start()
@@ -18,12 +22,31 @@ public class Pole : MonoBehaviour
     }
     private void OnMouseDown()
     {
-        setColor();
-        //manager.GetComponent<TullyMonster67>().AddPoints(points);
+        manager.GetComponent<TullyMonster67>().poleClicked(poleNum);
     }
-    private void setColor()
+    public List<Rings> getRings()
     {
-       GetComponent<SpriteRenderer>().color = new Color(200f/255f, 110f/255f, 110f/255f);
+        return rings;
+    }
+    public void setSelected(bool tf)
+    {
+        selected = tf;
+        //return true;
+    }
+    public bool isSelected()
+    {
+        return true;
+    }
+    public void setColor()
+    {
+        if(selected = false)
+        {
+            GetComponent<SpriteRenderer>().color = new Color(118f/255f, 118f/255f, 118f/255f);
+        }
+        else{
+            GetComponent<SpriteRenderer>().color = new Color(200f/255f, 110f/255f, 110f/255f);
+        }
+       
     
     }
 }
