e5412ce5267281897db53ad80f20f70d2b2b5edb
Shotobob <109097353+Shotobob@users.noreply.github.com>
2025-10-05 20:08:01 -0500

    dd

--- RushHour/Assets/Scripts/Car.cs
+++ RushHour/Assets/Scripts/Car.cs
@@ -9,16 +9,19 @@ public class Car : MonoBehaviour
     [SerializeField] bool vert; 
     [SerializeField] bool race; 
     public bool end = false;
-    
-    
+    //[SerializeField] bool win;
+
+
     [SerializeField] GameObject manager;
     private float ogx = 0;
     private float ogy = 0;
     private float ogz = 0;
-    
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        end = false;
+        //manager.GetComponent<TullyMonster67>().setWin(false);
         ogx = transform.position.x;
         ogy = transform.position.y;
         
@@ -28,77 +31,107 @@ public class Car : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (manager.GetComponent<TullyMonster67>().isWin() == true)
+        {
+            end = true;
+        }
         if (Input.GetKeyDown(KeyCode.R))
         {
+            if(end == true)
+            {
+                transform.position = new Vector3(ogx, ogy, transform.position.z);
+                manager.GetComponent<TullyMonster67>().winningText(false);
+
+                manager.GetComponent<TullyMonster67>().setWin(false);
+                manager.GetComponent<TullyMonster67>().setZero();
+                end = false;
+            }
             
-            transform.position = new Vector3(ogx, ogy, transform.position.z);
+
         }
+        if(race == true)
+        {
+            RaycastHit2D[] rays = Physics2D.RaycastAll(transform.position, Vector2.left, 1.2f);
+            if (rays.Length >= 3)
+            {
+                manager.GetComponent<TullyMonster67>().setWin(true);
+                manager.GetComponent<TullyMonster67>().winningText(true);
+                
+            }
+
+        }
+        
     }
     private void OnMouseDown()
     {
-        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        
-        if(race == true )
+        if (manager.GetComponent<TullyMonster67>().isWin() == false)
         {
-            RaycastHit2D[] rays = Physics2D.RaycastAll(transform.position, Vector2.left, 1.2f);
-            if(rays.Length == 1)
+            Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            if (race == true)
             {
-                transform.position = new Vector3(transform.position.x - 1.1f, transform.position.y, transform.position.z);
-                manager.GetComponent<TullyMonster67>().addMove();
+                RaycastHit2D[] rays = Physics2D.RaycastAll(transform.position, Vector2.left, 1.2f);
+                if (rays.Length == 1)
+                {
+                    transform.position = new Vector3(transform.position.x - 1.1f, transform.position.y, transform.position.z);
+                    manager.GetComponent<TullyMonster67>().addMove();
+
+                }
+
 
             }
-            
-            
-        }
-        else{
-            
-            if(vert == false)
+            else
             {
-                if(pos.x < transform.position.x)
+
+                if (vert == false)
                 {
-                    RaycastHit2D[] rays = Physics2D.RaycastAll(transform.position, Vector2.left, 2.2f);
-                    if(rays.Length == 1)
+                    if (pos.x < transform.position.x)
                     {
-                        transform.position = new Vector3(transform.position.x - 1.1f, transform.position.y, transform.position.z);
-                        manager.GetComponent<TullyMonster67>().addMove();
+                        RaycastHit2D[] rays = Physics2D.RaycastAll(transform.position, Vector2.left, 2.2f);
+                        if (rays.Length == 1)
+                        {
+                            transform.position = new Vector3(transform.position.x - 1.1f, transform.position.y, transform.position.z);
+                            manager.GetComponent<TullyMonster67>().addMove();
+                        }
+
                     }
-                    
+                    else
+                    {
+                        RaycastHit2D[] rays = Physics2D.RaycastAll(transform.position, Vector2.right, 2.2f);
+                        if (rays.Length == 1)
+                        {
+                            transform.position = new Vector3(transform.position.x + 1.1f, transform.position.y, transform.position.z);
+                            manager.GetComponent<TullyMonster67>().addMove();
+                        }
+
+                    }
                 }
                 else
                 {
-                    RaycastHit2D[] rays = Physics2D.RaycastAll(transform.position, Vector2.right, 2.2f);
-                    if(rays.Length == 1)
+                    if (pos.y < transform.position.y)
                     {
-                        transform.position = new Vector3(transform.position.x + 1.1f, transform.position.y, transform.position.z);
-                        manager.GetComponent<TullyMonster67>().addMove();
+                        RaycastHit2D[] rays = Physics2D.RaycastAll(transform.position, Vector2.down, 2.2f);
+                        if (rays.Length == 1)
+                        {
+                            transform.position = new Vector3(transform.position.x, transform.position.y - 1.1f, transform.position.z);
+                            manager.GetComponent<TullyMonster67>().addMove();
+                        }
+
                     }
-                    
-                }
-            }
-            else{
-                if(pos.y < transform.position.y)
-                {
-                    RaycastHit2D[] rays = Physics2D.RaycastAll(transform.position, Vector2.down, 2.2f);
-                    if(rays.Length == 1)
+                    else
                     {
-                        transform.position = new Vector3(transform.position.x , transform.position.y- 1.1f, transform.position.z);
-                        manager.GetComponent<TullyMonster67>().addMove();
+
+                        RaycastHit2D[] rays = Physics2D.RaycastAll(transform.position, Vector2.up, 2.2f);
+                        if (rays.Length == 1)
+                        {
+                            transform.position = new Vector3(transform.position.x, transform.position.y + 1.1f, transform.position.z);
+                            manager.GetComponent<TullyMonster67>().addMove();
+                        }
+
                     }
-                    
-                }
-                else
-                {
-                    
-                    RaycastHit2D[] rays = Physics2D.RaycastAll(transform.position, Vector2.up, 2.2f);
-                    if(rays.Length == 1)
-                    {
-                        transform.position = new Vector3(transform.position.x, transform.position.y + 1.1f, transform.position.z);
-                        manager.GetComponent<TullyMonster67>().addMove();
-                    }
-                    
                 }
             }
         }
-        
+
     }
 }
--- RushHour/Assets/Scripts/TullyMonster67.cs
+++ RushHour/Assets/Scripts/TullyMonster67.cs
@@ -5,21 +5,45 @@ using UnityEngine.UI;
 
 public class TullyMonster67 : MonoBehaviour
 {
-    [SerializeField] Text scoreText; 
+    [SerializeField] Text scoreText;
     private int moves = 0;
+    [SerializeField] Text winText;
+    private bool win = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        winText.enabled = false;
+        win = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         scoreText.text = "Move: " + moves;
+        
     }
     public void addMove()
     {
         moves = moves + 1;
+    }
+    public void winningText(bool win)
+    {
+        winText.enabled = win;
+
+    }
+    public void setWin(bool win2)
+    {
+        win = win2;
+
+    }
+    public bool isWin()
+    {
+        return win;
+
+    }
+    public void setZero()
+    {
+        moves = 0;
+
     }
 }
