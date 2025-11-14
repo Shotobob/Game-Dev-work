498b27ddbd9938d3f303404498ee4bcc3a761a8a
Shotobob <109097353+Shotobob@users.noreply.github.com>
2025-11-14 11:50:53 -0600

    wdwd

---
+++ Electric poles/Assets/Scripts/Tullymonster67.cs
@@ -0,0 +1,96 @@
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tullymonster67 : MonoBehaviour
+{
+
+    [SerializeField] List<Pole> poles;
+    public int selectedPole = -1;
+    [SerializeField] Text death;
+    [SerializeField] Text win;
+    public int movesNum = 0;
+    public bool end = false;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        death.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void poleClicked(int pole)
+    {
+        if (end == false)
+        {
+            double x = 0;
+            double y = -3.5;
+            if (selectedPole == -1)
+            {
+                selectedPole = pole;
+                poles[pole].setSelected(true);
+                //poles[pole].setColor();
+            }
+            else if (selectedPole == pole)
+            {
+                selectedPole = -1;
+                poles[pole].setSelected(false);
+                //poles[pole].setColor();
+            }
+            else
+            {
+                poles[selectedPole].setSelected(false);
+                //poles[selectedPole].setColor();
+
+                if (poles[selectedPole].getLights().Count != 0)
+                {
+                    if (poles[pole].getLights().Count == 0 || poles[selectedPole].getLights()[0].getWeight() < poles[pole].getLights()[0].getWeight())
+                    {
+
+                        for (int i = 0; i < poles[pole].getLights().Count; i++)
+                        {
+                            y += 0.8;
+                        }
+                        Light movedLight = poles[selectedPole].getLights()[0];
+                        if (pole == 0)
+                        {
+                            x = -6.5;
+                            movedLight.move((float)x, (float)y);
+
+                        }
+                        if (pole == 1)
+                        {
+                            x = 0;
+                            movedLight.move((float)x, (float)y);
+
+                        }
+                        if (pole == 2)
+                        {
+                            x = 6.5;
+                            movedLight.move((float)x, (float)y);
+                        }
+                        poles[pole].addLights(poles[selectedPole].getLights()[0]);
+                        poles[selectedPole].removeLights();
+                        movesNum++;
+
+                    }
+                    else
+                    {
+                        death.enabled = true;
+                    }
+
+                }
+                selectedPole = -1;
+                poles[pole].setSelected(false);
+
+
+            }
+        }
+
+        
+    }
+}
