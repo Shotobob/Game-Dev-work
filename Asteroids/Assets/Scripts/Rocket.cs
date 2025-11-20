7296effc50352a137e31edf98fa85bdd5037f4ef
Shotobob <109097353+Shotobob@users.noreply.github.com>
2025-11-20 11:50:30 -0600

    0'[=-'

--- Asteroids/Assets/Scripts/Rocket.cs
+++ Asteroids/Assets/Scripts/Rocket.cs
@@ -4,6 +4,10 @@ public class Rocket : MonoBehaviour
 {
     [SerializeField] GameObject manager;
     [SerializeField] GameObject Bullet;
+    [SerializeField] GameObject Lthrust;
+    [SerializeField] GameObject Rthrust;
+    [SerializeField] GameObject Bthrust;
+    [SerializeField] GameObject flash;
     bool up, down, left, right;
     [SerializeField] AudioSource bsound;
     Rigidbody2D rb;
@@ -11,25 +15,43 @@ public class Rocket : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        Lthrust.GetComponent<SpriteRenderer>().enabled= false;
+        Rthrust.GetComponent<SpriteRenderer>().enabled = false;
+        Bthrust.GetComponent<SpriteRenderer>().enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Lthrust.GetComponent<SpriteRenderer>().enabled= false;
+        Rthrust.GetComponent<SpriteRenderer>().enabled = false;
+        Bthrust.GetComponent<SpriteRenderer>().enabled = false;
+        
         if(Input.GetKey(KeyCode.W))
+        {
             rb.AddForce(transform.right);
+            Bthrust.GetComponent<SpriteRenderer>().enabled = true;
+        }
         if(Input.GetKey(KeyCode.A))
             transform.Rotate(0, 0, 1);
         if(Input.GetKey(KeyCode.S))
+        {
             rb.AddForce(-transform.right);
+            Lthrust.GetComponent<SpriteRenderer>().enabled = true;
+            Rthrust.GetComponent<SpriteRenderer>().enabled = true;
+        }
+            
         if(Input.GetKey(KeyCode.D))
             transform.Rotate(0, 0, -1);
+
         if(Input.GetKeyDown(KeyCode.Space))
         {
             GameObject b = Instantiate(Bullet, transform.position + transform.right*1.5f, transform.rotation);
             b.GetComponent<Rigidbody2D>().linearVelocity = b.transform.right *5f;
             bsound.Play();
+            flash.GetComponent<SpriteRenderer>().enabled = true;
         }
+        flash.GetComponent<SpriteRenderer>().enabled = false;
         if(transform.position.x > 9f)
         {
             transform.position = new Vector3(transform.position.x - 18f, transform.position.y, transform.position.y);
