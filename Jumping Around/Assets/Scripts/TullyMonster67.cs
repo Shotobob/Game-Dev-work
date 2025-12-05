968095ce060a81df9a1efad03442d6cdcdf2a1b3
Shotobob <109097353+Shotobob@users.noreply.github.com>
2025-12-05 11:50:44 -0600

    wddwd

--- Jumping Around/Assets/Scripts/TullyMonster67.cs
+++ Jumping Around/Assets/Scripts/TullyMonster67.cs
@@ -20,12 +20,29 @@ public class TullyMonster67 : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.D))
             right = true;
         if(Input.GetKeyUp(KeyCode.A))
-            left = true;
+            left = false;
         if(Input.GetKeyUp(KeyCode.D))
-            right = true;
-        /*if(Input.GetKeyUp(KeyCode.Space))
-            //rb.linearvlocity = (new Vector2(rb.velocity.x, jumpforce));*/
-
-
+            right = false;
+        if(Input.GetKeyDown(KeyCode.Space) == true && isGrounded() == true)
+            rb.linearVelocity = (new Vector2(rb.linearVelocity.x, jumpforce));
+    }
+    public bool isGrounded()
+    {
+        bool ig = false;
+        Vector2 castFrom = new Vector2(transform.position.x, transform.position.y - GetComponent<SpriteRenderer>().bounds.size.y / 2 - 0.01f);
+        RaycastHit2D hit = Physics2D.Raycast(castFrom, Vector2.down, .1f);
+        //Debug.DrawRay(castFrom, (Vector2.down*.1f), )
+        if(hit.transform != null && hit.transform.tag == "g")
+            ig = true;
+        return ig;
+    }
+    public void FixedUpdate()
+    {
+        float h = 0f;
+        if(left)
+            h -= speed;
+        if(right)
+            h += speed;
+        rb.linearVelocity = new Vector2(h, rb.linearVelocity.y);
     }
 }
