705f65d5da3ecd19219291685658d833066917cc
Shotobob <109097353+Shotobob@users.noreply.github.com>
2025-12-11 11:51:04 -0600

    dd

--- Othello/Assets/Scripts/Pieces.cs
+++ Othello/Assets/Scripts/Pieces.cs
@@ -1,16 +1,43 @@
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
 public class Pieces : MonoBehaviour
 {
+
+     [SerializeField] GameObject black;
+     [SerializeField] GameObject white;
+
+    private bool isblack = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        black.GetComponent<SpriteRenderer>().sortingOrder= 3;
+        white.GetComponent<SpriteRenderer>().sortingOrder= 0;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+    public void flip()
+    {
+        if(isblack == true)
+        {
+            black.GetComponent<SpriteRenderer>().sortingOrder= 0;
+            white.GetComponent<SpriteRenderer>().sortingOrder= 3;
+
+            
+        }
+        else{
+            black.GetComponent<SpriteRenderer>().sortingOrder= 3;
+            white.GetComponent<SpriteRenderer>().sortingOrder= 0;
+
+        }
         
     }
 }
--- Othello/Assets/Scripts/TullyMonster67.cs
+++ Othello/Assets/Scripts/TullyMonster67.cs
@@ -13,26 +13,54 @@ public class TullyMonster67 : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        pieces.Add(piece);
-    }
+        
+        GameObject o = Instantiate(piece);
+        //Rigidbody2D rb = o.GetComponent<Rigidbody2D>();
+        pieces.Add(o);
+        o.transform.position = new Vector2(0.51f, 0.51f);
+        
 
+        o = Instantiate(piece);
+        //Rigidbody2D rb = o.GetComponent<Rigidbody2D>();
+        pieces.Add(o);
+        o.transform.position = new Vector2(-0.51f, 0.51f);
+        o.GetComponent<Pieces>().flip();
+
+        // o.GetComponent<Pieces>().flip();
+         o = Instantiate(piece);
+        //Rigidbody2D rb = o.GetComponent<Rigidbody2D>();
+        pieces.Add(o);
+        o.transform.position = new Vector2(0.51f, -0.51f);
+        
+
+        // o.GetComponent<Pieces>().flip();
+        o = Instantiate(piece);
+        //Rigidbody2D rb = o.GetComponent<Rigidbody2D>();
+        pieces.Add(o);
+        o.transform.position = new Vector2(-0.51f, -0.51f);
+        o.GetComponent<Pieces>().flip();
+        
+        o = Instantiate(piece);
+        //Rigidbody2D rb = o.GetComponent<Rigidbody2D>();
+        pieces.Add(o);
+        
+
+
+    }
     // Update is called once per frame
     void Update()
     {
+        if(placed == false)
+        {
+            move();
+        }
+        
+       
+    }
+    public void move()
+    {
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         pieces[pieces.Count - 1].transform.position = new Vector2(mousePos.x, mousePos.y);
-        //correct();
-        if (Input.GetKeyDown(KeyCode.Mouse0) && placed == false)
-        {
-            RaycastHit2D[] rays = Physics2D.RaycastAll(pieces[pieces.Count - 1].transform.position, Vector2.down, 1.5f);
-            {
-                if(rays.Length <= 1)
-                {
 
-                    placed = true;
-                }
-            }
-
-        }
     }
 }
